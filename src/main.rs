92a6963b484e43a3c00bd99361846d791c40ec4f
Rudi Horn <dyn-git@rudi-horn.de>
2018-03-19 16:33:29 +0000

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -78,12 +78,7 @@ app! {
 
 #[inline(never)]
 fn init(p: init::Peripherals, _r : init::Resources) -> init::LateResources {
-    iprintln!("SPI Example");
-
-    /* let timer = Timer(p.TIM1);
-
-    timer.init(FREQUENCY.invert(), p.RCC);
-    timer.resume(); */
+    iprintln!("Skeleton");
 
     let rcc = Rcc(&p.device.RCC);
     let rcc_periph = rcc.get_peripherals();
@@ -101,40 +96,6 @@ fn init(p: init::Peripherals, _r : init::Resources) -> init::LateResources {
     let afio = Afio(&p.device.AFIO);
     let afio_periph = afio.get_peripherals();
 
-    // initialize the temperature sensor
-    tempsensor::init_temp(&p.device.SPI2, pinsb.12, pinsb.13, pinsb.14, pinsb.15);
-
-    // initialize the screen
-    screen::init_screen(&p.device.I2C1, pinsb.8, pinsb.9, afio_periph.i2c1);
-    
-    iprintln!("Finished initialization");
-
-    // initialize external timer interrupt
-    unsafe {
-        // set line 5 to A0
-        p.device.AFIO.exticr3.modify(|_, w| w.exti8().bits(0));
-        // enable interrupt on line 8
-        p.device.EXTI.imr.modify(|_, w| w.mr8().set_bit());
-        // enable fall trigger on line 8
-        p.device.EXTI.ftsr.modify(|_, w| w.tr8().set_bit());
-    }
-
-    let tim2 = rcc_periph.tim2.enable_tim2().reset();
-
-
-    {
-        // initialize timer
-        let tim = &p.device.TIM2;
-        // set prescaler to f_apb / 800
-        tim.psc.modify(|_, w| w.psc().bits(799));
-        // set auto reset register to 10
-        tim.arr.modify(|_, w| w.arr().bits(10));
-        // enable interrupt
-        tim.dier.modify(|_, w| w.uie().set_bit());
-        // enable counter
-        tim.cr1.modify(|_, w| w.cen().set_bit());
-    }
-
     init::LateResources {
         I2C1: p.device.I2C1,
         SPI2_REG: p.device.SPI2,
@@ -146,26 +107,6 @@ fn init(p: init::Peripherals, _r : init::Resources) -> init::LateResources {
 
 
 fn idle(t: &mut Threshold, r: idle::Resources) -> ! {
-    screen::set_address_mode(t, &r.I2C1);
-    screen::set_address(t, &r.I2C1, 0, 0);
-
-
-    for _i in 0..64 {
-        ssd1306::write_data(t, &r.I2C1, &[0; 8]);
-        ssd1306::wait_buffer();
-    } 
-
-    screen::set_address(t, &r.I2C1, 0, 0);
-    screen::write_number(t, &r.I2C1, 10);
-
-    r.SPI2_REG.claim(t, |spi, _t| {
-        let spi = Spi(&*spi);
-        unsafe {
-           SPI_RES.start_write(0x80, 0b11010001, &spi);
-           //SPI_RES.start_read(0x0, &spi);
-        }
-    });
-
     iprintln!("Entering idle loop...");
 
     loop {
@@ -180,110 +121,17 @@ pub enum ReadState {
 }
 
 fn i2c_ev_interrupt(t: &mut Threshold, r: I2C1_EV::Resources) {
-    let i2c = r.I2C1;
-    i2c.claim(t, |i2c1, _t| {
-        let i2c = I2c(i2c1);
-        ssd1306::event_interrupt(&i2c);
-    });
 }
 
 fn i2c_er_interrupt(_t: &mut Threshold, r: I2C1_ER::Resources) {
-    let a = (r.I2C1).sr1.read();
-
-    iprintln!("er {} / AF {}", a.bits(), a.af().bit_is_set());
-    rtfm::bkpt();
 }
 
-static mut READ_STATE : ReadState = ReadState::Conf;
-static mut LAST_TEMP : u16 = 0;
-static mut LAST_READ : u64 = 0;
 
 fn spi_interrupt(t: &mut Threshold, r: SPI2::Resources) {
-    let spi_res = unsafe { &mut SPI_RES };
-    let spi = Spi(&*r.SPI2_REG);
-
-    spi_res.process_int(&spi);
-
-    unsafe {
-        match SPI_RES.state {
-            SpiState::Finished =>
-            {
-                match READ_STATE {
-                    ReadState::Conf => {
-                        SPI_RES.start_read(0x02, &spi);
-                        READ_STATE = ReadState::Lsb;
-                    }
-                    ReadState::Lsb => {
-                        SPI_RES.start_read(0x01, &spi);
-                        READ_STATE = ReadState::Msb(SPI_RES.result);
-                    }
-                    ReadState::Msb(lsb) => {
-                        let val : u16 = ((SPI_RES.result as u16) << 8) | (lsb as u16);
-                        if val != LAST_TEMP {
-                            LAST_TEMP = val;
-                            iprint!("val: {} ", val);
-                            let conv = ((val >> 1) as u32 * 43234) >> 15;
-                            let temp = temp_conversion::lookup_temperature(conv as u16);
-
-                            screen::set_address(t, &r.I2C1, 0, 0);
-                            // ensure the number is completely covered by making sure 
-                            // we always print 5 digits
-                            if temp < 10000 {
-                                screen::write_empty_digit(t, &r.I2C1);
-                            }
-                            screen::write_number(t, &r.I2C1, temp / 100);
-                            screen::write_dot(t, &r.I2C1);
-                            screen::write_number(t, &r.I2C1, temp % 100);
-                            iprintln!("-> {}", temp);
-                        }
-
-                        // read next value
-                        READ_STATE = ReadState::Lsb;
-                        // SPI_RES.start_read(0x02, &spi);
-                    }
-                }
-                // SPI_RES.state = SpiState::Idle;  
-            }
-            _ => {}
-        }
-    }
 }
 
-static mut CNTR : u64 = 0;
 
-fn timer2_interrupt(t: &mut Threshold, r: TIM2::Resources) {
-    let tim2 = &*r.TIM2_R;
-    tim2.sr.modify(|_, w| w.uif().clear_bit());
-
-    unsafe {
-        CNTR += 1;
-    }
-
-    if unsafe { CNTR } % 1000 == 0 {
-        iprintln!("ext {}", tim2.sr.read().bits());
-        screen::set_address(t, &r.I2C1, 0, 1);
-    }
-    if unsafe { CNTR } % 2000 == 0 {
-        screen::write_dot(t, &r.I2C1);
-    } else if unsafe { CNTR } % 2000 == 1000 {
-        screen::write_empty_digit(t, &r.I2C1);
-    }
-
-}
+fn timer2_interrupt(t: &mut Threshold, r: TIM2::Resources) {}
 
 fn external_interrupt(_t: &mut Threshold, r: EXTI9_5::Resources) {
-    unsafe {
-        let c = CNTR;
-        let took = c - LAST_READ;
-        iprintln!("hz {} {}", 1000 / took, took);
-        LAST_READ = c;
-    }
-
-    let spi = Spi(&*r.SPI2_REG);
-
-    if r.EXTI.pr.read().pr8().bit_is_set() {
-        unsafe { SPI_RES.start_read(0x02, &spi); }
-
-        r.EXTI.pr.modify(|_, w| w.pr8().set_bit());
-    }
 }
